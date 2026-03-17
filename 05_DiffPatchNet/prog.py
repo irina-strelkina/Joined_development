80ee526277508f10ef172e22075b7585be369851
Irina <irina.s.irina.s19@gmail.com>
2026-03-17 21:10:01 +0300

    added login

--- 05_DiffPatchNet/prog.py
+++ 05_DiffPatchNet/prog.py
@@ -1,29 +1,43 @@
 import asyncio
+import cowsay
 
 clients = {}
 
 async def chat(reader, writer):
-    me = "{}:{}".format(*writer.get_extra_info('peername'))
-    print(me)
-    clients[me] = asyncio.Queue()
+    me = None
     send = asyncio.create_task(reader.readline())
-    receive = asyncio.create_task(clients[me].get())
+    receive = None
     while not reader.at_eof():
-        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        tasks = [send]
+        if me is not None:
+            if receive is None:
+                receive = asyncio.create_task(clients[me].get())
+            tasks.append(receive)
+        done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
         for q in done:
             if q is send:
                 send = asyncio.create_task(reader.readline())
-                for out in clients.values():
-                    if out is not clients[me]:
-                        await out.put(f"{me} {q.result().decode().strip()}")
+                line = q.result().decode().strip()
+                if me is None:
+                    cmd = line.split(maxsplit=1)
+                    if len(cmd) == 2 and cmd[0] == "login":
+                        cow = cmd[1]
+                        if cow in cowsay.list_cows() and cow not in clients:
+                            me = cow
+                            clients[me] = asyncio.Queue()
+                else:
+                    for out in clients.values():
+                        if out is not clients[me]:
+                            await out.put(f"{me} {line}")
             elif q is receive:
                 receive = asyncio.create_task(clients[me].get())
                 writer.write(f"{q.result()}\n".encode())
                 await writer.drain()
     send.cancel()
-    receive.cancel()
-    print(me, "DONE")
-    del clients[me]
+    if receive is not None:
+        receive.cancel()
+    if me is not None:
+        del clients[me]
     writer.close()
     await writer.wait_closed()
 
