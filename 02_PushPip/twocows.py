34efac1b46a268dd94cef835ee4afec3a28329ec
Irina <irina.s.irina.s19@gmail.com>
2026-03-29 14:55:43 +0300

    final version

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -3,7 +3,9 @@ import cowsay
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("message")
+parser.add_argument("message1", nargs="?")
+parser.add_argument("message2", nargs="?")
+
 parser.add_argument("-f", dest="cow", default="default")
 parser.add_argument("-e", dest="eyes", default="oo")
 parser.add_argument("-T", dest="tongue", default="  ")
@@ -20,13 +22,29 @@ args = parser.parse_args()
 if args.list_cows:
     print("\n".join(cowsay.list_cows()))
 else:
-    print(
-        cowsay.cowsay(
-            message=args.message,
-            cow=args.cow,
-            eyes=args.eyes,
-            tongue=args.tongue,
-            width=args.width,
-            wrap_text=not args.no_wrap,
-        )
-    )
+    first = cowsay.cowsay(
+        message=args.message1,
+        cow=args.cow,
+        eyes=args.eyes,
+        tongue=args.tongue,
+        width=args.width,
+        wrap_text=not args.no_wrap,
+    ).splitlines()
+
+    second = cowsay.cowsay(
+        message=args.message2,
+        cow=args.cow2,
+        eyes=args.eyes2,
+        width=args.width,
+        wrap_text=not args.no_wrap2,
+    ).splitlines()
+
+    first_width = max(len(line) for line in first)
+    second_width = max(len(line) for line in second)
+    height = max(len(first), len(second))
+
+    first = [" " * first_width] * (height - len(first)) + [line.ljust(first_width) for line in first]
+    second = [" " * second_width] * (height - len(second)) + [line.ljust(second_width) for line in second]
+
+    for left, right in zip(first, second):
+        print(left + "  " + right)
