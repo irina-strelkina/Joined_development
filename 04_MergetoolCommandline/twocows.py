07435f162e5c634eb4f077bc51e95e9d5fe39c6f
Irina <irina.s.irina.s19@gmail.com>
2026-03-29 18:02:52 +0300

    added make_bubble

--- 04_MergetoolCommandline/twocows.py
+++ 04_MergetoolCommandline/twocows.py
@@ -48,6 +48,21 @@ class TwoCowsShell(cmd.Cmd):
             return
         print("\n".join(cowsay.list_cows()))
 
+    def do_make_bubble(self, arg):
+        """Create a bubble for message"""
+        args = shlex.split(arg)
+        if not args:
+            print("make_bubble requires a message")
+            return
+
+        message = args[0]
+        params = parse_kv_tokens(args[1:]) if len(args) > 1 else {}
+
+        width = int(params.get("width", 40))
+        wrap_text = params.get("wrap_text", "True").lower() not in {"false", "0", "no"}
+
+        print(cowsay.make_bubble(message, width=width, wrap_text=wrap_text))
+
     def do_EOF(self, arg):
         return True
 
