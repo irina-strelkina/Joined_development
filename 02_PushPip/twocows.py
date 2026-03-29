4d7cf496578863a116f6b51e63cc67f237532c6b
Irina <irina.s.irina.s19@gmail.com>
2026-03-29 13:22:32 +0300

    added flags

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -11,6 +11,10 @@ parser.add_argument("-W", dest="width", type=int, default=40)
 parser.add_argument("-n", dest="no_wrap", action="store_true")
 parser.add_argument("-l", dest="list_cows", action="store_true")
 
+parser.add_argument("-F", dest="cow2", default="default")
+parser.add_argument("-E", dest="eyes2", default="oo")
+parser.add_argument("-N", dest="no_wrap2", action="store_true")
+
 args = parser.parse_args()
 
 if args.list_cows:
