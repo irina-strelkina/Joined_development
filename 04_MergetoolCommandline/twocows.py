7082484da6ad2350077641bad4aa92c18a9d8211
Irina <irina.s.irina.s19@gmail.com>
2026-03-29 17:41:20 +0300

    added shlex

--- 04_MergetoolCommandline/twocows.py
+++ 04_MergetoolCommandline/twocows.py
@@ -1,5 +1,40 @@
 import argparse
+import shlex
 import cowsay
+
+
+def parse_kv_tokens(tokens):
+    params = {}
+    for token in tokens:
+        if "=" not in token:
+            raise ValueError(f"Expected parameter=value, got: {token}")
+        key, value = token.split("=", 1)
+        params[key] = value
+    return params
+
+
+def parse_shlex_command(line):
+    tokens = shlex.split(line)
+    if not tokens:
+        return None
+
+    message = tokens[0]
+    cow = "default"
+
+    if len(tokens) >= 2 and "=" not in tokens[1]:
+        cow = tokens[1]
+        extra_tokens = tokens[2:]
+    else:
+        extra_tokens = tokens[1:]
+
+    extra = parse_kv_tokens(extra_tokens) if extra_tokens else {}
+
+    return {
+        "message": message,
+        "cow": cow,
+        "extra": extra,
+    }
+
 
 parser = argparse.ArgumentParser()
 
