d3e22e36e749ee0ca6441524a3e0e7f7dc979978
Irina <irina.s.irina.s19@gmail.com>
2026-03-29 13:16:09 +0300

    added basic functionality

---
+++ 02_PushPip/twocows.py
@@ -0,0 +1,28 @@
+import argparse
+import cowsay
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("message")
+parser.add_argument("-f", dest="cow", default="default")
+parser.add_argument("-e", dest="eyes", default="oo")
+parser.add_argument("-T", dest="tongue", default="  ")
+parser.add_argument("-W", dest="width", type=int, default=40)
+parser.add_argument("-n", dest="no_wrap", action="store_true")
+parser.add_argument("-l", dest="list_cows", action="store_true")
+
+args = parser.parse_args()
+
+if args.list_cows:
+    print("\n".join(cowsay.list_cows()))
+else:
+    print(
+        cowsay.cowsay(
+            message=args.message,
+            cow=args.cow,
+            eyes=args.eyes,
+            tongue=args.tongue,
+            width=args.width,
+            wrap_text=not args.no_wrap,
+        )
+    )
